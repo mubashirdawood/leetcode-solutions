c6d341d89e98913f0c58acbf0379068fc6348001
Mubashir Dawood <mubashirdawood05@gmail.com>
2026-07-07 01:27:55 +0500

    Time: 7 ms (97.55%), Space: 48.3 MB (98.46%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,16 +1,24 @@
-import java.util.Arrays;
-
 class Solution {
     public int arrayPairSum(int[] nums) {
-     
-        Arrays.sort(nums);
-        
-        int maxSum = 0;
-        
-        for (int i = 0; i < nums.length; i += 2) {
-            maxSum += nums[i];
+        int[] count = new int[20001];
+
+        for (int num : nums) {
+            count[num + 10000]++;
         }
-        
-        return maxSum;
+
+        int sum = 0;
+        boolean take = true;
+
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                if (take) {
+                    sum += i - 10000;
+                }
+                take = !take;
+                count[i]--;
+            }
+        }
+
+        return sum;
     }
 }
